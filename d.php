90d7984bc9c0e79c3f13a629a343e8f0741492b2
onetarek <onetarek@gmail.com>
2016-02-17 01:02:42 +0600

    Test

---
+++ d.php
@@ -0,0 +1,15 @@
+<?php
+//dream
+	$x;
+	for( $x = 0; $x <= 100 ; $x++ ){
+		if( $x % 2 == 0 )
+		{
+			echo "Even";
+		}
+		else
+		{
+			echo "Odd";
+		}
+		
+	}
+?>
---
+++ s.php
@@ -0,0 +1,14 @@
+<?php
+//sub
+	$x;
+	for( $x = 0; $x <=100; $x++){
+		if( $x % 2 ==0 )
+		{
+			echo "Even";
+		}
+		else
+		{
+			echo "Odd";
+		}
+	}
+?>
